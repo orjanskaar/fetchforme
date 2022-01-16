b2b3aa3e1349c729ad714c688b31fba9ea9ed676
orjanskaar <orjanskaar@gmail.com>
2022-01-16 21:47:50 +0100

    v

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-exports.sayhey = () => {
-    console.log('Hello there!')
+import React from 'react'
+
+function Cookie() {
+    return (
+        <Box>
+            <h1>pouhouihiuhiuhiuhiuhiuhihqwdpj</h1>
+            <Box sx={{whiteSpace: 'pre-wrap', fontSize: '1rem'}} as="pre">
+                owiejwej
+            </Box>
+        </Box>
+    )
 }
+
+export default Cookie()
